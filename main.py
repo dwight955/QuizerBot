009dfd9f0580907d0b30808e19b673b52dd18eb9
Dwight Huanca <dwightht.anthony955@gmail.com>
2022-09-05 23:15:17 -0500

    Primer Commit - Configuracion basica

---
+++ main.py
@@ -0,0 +1,30 @@
+import asyncio
+import logging
+from telegram import __version__ as TG_VER
+
+from telegram import(
+    Poll,
+    Update
+)
+from telegram.ext import(
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    PollAnswerHandler,
+    PollHandler,
+)
+import config
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the Bot")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(config.VariableConst.MY_TOKEN_BOT).build()
+
+    application.add_handler(CommandHandler('start',start))
+
+    application.run_polling()
