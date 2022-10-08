75a2713aff2d6fc093a24a3ffb3763dbcbb6d8f6
flowaraberd <flowaraberd@gmail.com>
2022-10-08 02:34:16 -0300

    Se agrego nuevas informaciones

--- controller/answer/answer_inline_keyboard.py
+++ controller/answer/answer_inline_keyboard.py
@@ -92,6 +92,7 @@ async def receive_inline_keyboard(update: Update, context:ContextTypes.DEFAULT_T
                                                "Lo sentimos ya no hay mas preguntas!\n\nPuede jugar el otro juego\n\n /poll")
 
     if answer_option in ('finish-poll', 'finish-quiz'):
+        user_data = get_user_data(id_user)
         result = 'Resultado'.center(50, '-')
         text = f'*{result}*\n\nCant. de pregunta respondida: *{user_data["questions_answered"]}/{len_question_total}*\n' \
                f'Cant. de quiz: *{user_data["quizs_answered"]}/{len_question_quiz}*\nCant. de poll: *{user_data["polls_answered"]}/{len_question_poll}*\n' \
