9908da281f077b43b9f3a0cb2fbe03973d52c398
flowaraberd <67209635+flowaraberd@users.noreply.github.com>
2022-12-31 00:00:02 -0300

    Se creo el archivo constant.py

---
+++ generalVariable/constant.py
@@ -0,0 +1,5 @@
+class CONSTANT:
+    MY_TOKEN_BOT = 'MY_TOKEN'
+    # Esta variable es donde se colocará la cantidad de puntos que quiere
+    # que le coloque al usuario cada vez que responda de forma correcta.
+    GIVE_POINTS = 5
