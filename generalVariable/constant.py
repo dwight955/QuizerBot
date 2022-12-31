fc06ce07912c7ef38966b308c06e67ee6474bd0e
flowaraberd <flowaraberd@gmail.com>
2022-12-30 23:47:25 -0300

    Se elimino el archivo userId del staging area

--- generalVariable/constant.py
+++
@@ -1,5 +0,0 @@
-class CONSTANT:
-    # Este es un nuevo Bot!, por si el otro ya no te funciona
-    MY_TOKEN_BOT = '5700048053:AAEfQNHkIgzLsdpDLyyV1Wnwjmbkeg5nXQs'
-    GIVE_POINTS = 5
-    NUMBER_CHAR_TO_CUT = 2
