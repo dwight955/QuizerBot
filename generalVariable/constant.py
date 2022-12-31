9e6cef210a5018bbc62d3dfd0a6af7f9f0b5f950
Dwight Huanca <dwightht.anthony955@gmail.com>
2022-12-30 22:30:00 -0500

    Se agrego mi TOKEN

--- generalVariable/constant.py
+++ generalVariable/constant.py
@@ -1,5 +1,5 @@
 class CONSTANT:
-    MY_TOKEN_BOT = 'MY_TOKEN'
+    MY_TOKEN_BOT = '5700048053:AAEfQNHkIgzLsdpDLyyV1Wnwjmbkeg5nXQs'
     # Esta variable es donde se colocará la cantidad de puntos que quiere
     # que le coloque al usuario cada vez que responda de forma correcta.
     GIVE_POINTS = 5
