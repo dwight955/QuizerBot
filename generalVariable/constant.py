255f1502c38a8cc6f50581826e879f3327b6d0c0
Dwight Huanca <dwightht.anthony955@gmail.com>
2022-12-30 21:07:24 -0500

    Se agrego el archivo requirements

--- generalVariable/constant.py
+++ generalVariable/constant.py
@@ -1,5 +1,5 @@
 class CONSTANT:
     # Este es un nuevo Bot!, por si el otro ya no te funciona
-    MY_TOKEN_BOT = '5391665339:AAEIFz4YwzFLiGrOwVeVb5YMaI_uPnj9fF8'
+    MY_TOKEN_BOT = '5700048053:AAEfQNHkIgzLsdpDLyyV1Wnwjmbkeg5nXQs'
     GIVE_POINTS = 5
     NUMBER_CHAR_TO_CUT = 2
