f29db015722e07f28708bccc09a05b3b1ed276b2
flowaraberd <flowaraberd@gmail.com>
2023-01-06 23:15:26 -0300

    Se agregaron las preguntas del poll

--- game/questions/question_poll.py
+++ game/questions/question_poll.py
@@ -1,72 +1,73 @@
 question_game_poll = [
     {
-        "question": "¿0 CUAL ES LA PREGUNTA?",
-        "options": ["A","B","C","D"],
-        "index_correct_answer": [1,2,3],
+        "question": "¿Qué tecnología se utilizan principalmente en el Front-end?",
+        "options": ["JavaScript","React","HTML","CSS"],
+        "index_correct_answer": [0, 2, 3],
         "voteAnonymous": False,
         "allowsMultipleAnswers": True
     },
     {
-        "question": "¿1 CUAL ES LA PREGUNTA?",
-        "options": ["A","B","C","D"],
-        "index_correct_answer": [1,2,3],
+        "question": "¿Cuáles son los colores de la bandera de China?",
+        "options": ["Amarillo","Rojo","Blanco","Azul"],
+        "index_correct_answer": [0, 1],
         "voteAnonymous": False,
         "allowsMultipleAnswers": True
     },
     {
-        "question": "¿2 CUAL ES LA PREGUNTA?",
-        "options": ["A","B","C","D"],
-        "index_correct_answer": [1,2,3],
+        "question": "¿Qué idiomas son oficiales en Canadá?",
+        "options": ["Francés","Inglés","Chino","Alemán"],
+        "index_correct_answer": [0, 3],
         "voteAnonymous": False,
         "allowsMultipleAnswers": True
     },
     {
-        "question": "¿3 CUAL ES LA PREGUNTA?",
-        "options": ["A","B","C","D"],
-        "index_correct_answer": [1,2,3],
+        "question": "¿Cuáles son los colores de la bandera de Italia?",
+        "options": ["rojo","amarillo","blanco","verde"],
+        "index_correct_answer": [0, 2, 3],
         "voteAnonymous": False,
         "allowsMultipleAnswers": True
     },
     {
-        "question": "¿4 CUAL ES LA PREGUNTA?",
-        "options": ["A","B","C","D"],
-        "index_correct_answer": [1,2,3],
+        "question": "¿Cuáles son los colores de la bandera de Alemania?",
+        "options": ["Rojo","Negro","Blanco","Amarillo"],
+        "index_correct_answer": [0, 1, 3],
         "voteAnonymous": False,
         "allowsMultipleAnswers": True
     },
     {
-        "question": "¿5 CUAL ES LA PREGUNTA?",
-        "options": ["A","B","C","D"],
-        "index_correct_answer": [1,2,3],
+        "question": "¿Qué idiomas son oficiales en Suiza?",
+        "options": ["alemán","francés","italiano","inglés"],
+        "index_correct_answer": [0, 1, 2],
         "voteAnonymous": False,
         "allowsMultipleAnswers": True
     },
     {
-        "question": "¿6 CUAL ES LA PREGUNTA?",
-        "options": ["A","B","C","D"],
-        "index_correct_answer": [1,2,3],
+        "question": "¿Quiénes son los autores de las famosas obras literarias \"Matar a un ruiseñor\" y \"La casa de los espíritus\"?",
+        "options": ["Harper Lee","Jorge Luis Borges","Mario Vargas Llosa","Isabel Allende"],
+        "index_correct_answer": [0, 3],
         "voteAnonymous": False,
         "allowsMultipleAnswers": True
     },
     {
-        "question": "¿7 CUAL ES LA PREGUNTA?",
-        "options": ["A","B","C","D"],
-        "index_correct_answer": [1,2,3],
+        "question": "¿Cuáles son los colores de la bandera de España?",
+        "options": ["rojo","amarillo","azul","verde"],
+        "index_correct_answer": [0, 1],
         "voteAnonymous": False,
         "allowsMultipleAnswers": True
     },
     {
-        "question": "¿8 CUAL ES LA PREGUNTA?",
-        "options": ["A","B","C","D"],
-        "index_correct_answer": [1,2,3],
+        "question": "¿Qué idiomas se hablan en Italia?",
+        "options": ["Italiano","Toscano","Español","Inglés"],
+        "index_correct_answer": [0, 1],
         "voteAnonymous": False,
         "allowsMultipleAnswers": True
     },
     {
-        "question": "¿9 CUAL ES LA PREGUNTA?",
-        "options": ["A","B","C","D"],
-        "index_correct_answer": [1,2,3],
+        "question": "¿Cuáles son los países más poblados del mundo?",
+        "options": ["China", "India", "Indonesia", "Estados Unidos"],
+        "index_correct_answer": [0, 1, 3],
         "voteAnonymous": False,
         "allowsMultipleAnswers": True
     },
 ]
+
